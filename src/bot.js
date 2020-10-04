15708e681d614e22af42c15b8669852e1849a1e3
Joan <54076370+Joanpixer@users.noreply.github.com>
2020-10-04 14:58:12 +0200

    1.0

---
+++ src/bot.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { Client } = require('discord.js');
+const client = new Client();
+const createCaptcha = require('./captcha');
+const fs = require('fs').promises;
+client.login(process.env.token);
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('guildMemberAdd', async member => {
+    const captcha = await createCaptcha();
+    try {
+        const msg = await member.send('', {
+            files: [{
+                attachment: `${__dirname}/captchas/${captcha}.png`,
+                name: `${captcha}.png`
+            }]
+        });
+        try {
+            const filter = m => {
+                if(m.author.bot) return;
+                if(m.author.id === member.id && m.content === captcha) return true;
+                else {
+                    m.channel.send('Incorrecto.');
+                    return false;
+                }
+            };
+            const response = await msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time']});
+            if(response) {
+                await msg.channel.send('Has sido verificado!');
+                await member.roles.add('736001778121965749');
+                await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
+                    .catch(err => console.log(err));
+            }
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+});
